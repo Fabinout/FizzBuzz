02395b551008395a3e0797183ec7fd09fdf70293
Fabien Lamarque <fabien.lamarque+git@gmail.com>
2014-12-30 19:17:10 +0100

    first commit

--- run.sh
+++ run.sh
@@ -1,14 +1,10 @@
+#!/bin/sh
 #mvn exec:java -Dexec.mainClass="com.FizzBuzz"
 
-#echo "compiling..."
-pwd
+echo "compiling..."
+
 javac -sourcepath src -d bin -cp lib/javassist.jar src/main/com/reflective/FizzBuzz.java
 javac -sourcepath src -d bin -cp lib/javassist.jar src/main/com/reflective/Setup.java
-##javac -sourcepath src ../../src/java/*.java
-#
 java -cp lib/javassist.jar:bin reflective/Setup temp
-#java -cp lib/javassist.jar:bin Setup temp
-#java -cp lib/javassist.jar:bin net.thecodersbreakfast.codestory.foobarqix.Setup temp
-#
-echo "Launching the FooBarQix challenge..."
+echo "Launching the FUZZBIZZ INTERVIEW..."
 java -cp bin -Xbootclasspath/p:temp reflective.FizzBuzz
