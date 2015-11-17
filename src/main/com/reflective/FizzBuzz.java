12403f9d6cffe9fe4f9e6ce7d2ad76184c64ec70
Fabien Lamarque <fabien.lamarque+git@gmail.com>
2015-11-17 13:59:38 +0100

    Pas besoin de Integer.toString

--- src/main/com/reflective/FizzBuzz.java
+++ src/main/com/reflective/FizzBuzz.java
@@ -7,7 +7,7 @@ public class FizzBuzz {
 
     public static void main(String[] args) {
         for (int i = 1; i <= 100; i++) {
-            System.out.println(Integer.toString(i));
+            System.out.println(i);
         }
     }
 
